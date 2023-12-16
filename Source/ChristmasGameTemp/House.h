a7a9d4c545261fc24c17297719ea402d4e31ef9a
Sayna <nwott1@gmail.com>
2023-12-16 18:03:56 -0500

    House detects player collision

---
+++ Source/ChristmasGameTemp/House.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "House.generated.h"
+
+UCLASS()
+class CHRISTMASGAMETEMP_API AHouse : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHouse();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Gets called when player is in box collider of house
+	UFUNCTION(BlueprintCallable, Category="House")
+	void OnPlayerColliderOverlap(AActor* otherActor);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
